1d5e6f11667b8109205d40b6c96b2d0705c355a5
kubaracek <me@jakubracek.net>
2020-02-21 00:08:53 +0100

    Useful stuff

---
+++ src/Utils.elm
@@ -0,0 +1,24 @@
+module Utils exposing (cartesianMap, flip, wrap)
+
+
+flip : (a -> b -> c) -> b -> a -> c
+flip function argB argA =
+    function argA argB
+
+
+wrap min max n =
+    if n < min then
+        wrap min max <| n + (max - min)
+
+    else if n >= max then
+        wrap min max <| n - (max - min)
+
+    else
+        n
+
+
+cartesianMap : (a -> b -> c) -> List a -> List b -> List (List c)
+cartesianMap function rows columns =
+    flip function
+        |> flip List.map columns
+        |> List.map (flip List.map rows)
