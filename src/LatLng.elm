af454efb3b842f3bc43843f18232b8c0107aa0ee
kubaracek <me@jakubracek.net>
2020-02-21 17:11:45 +0100

    Opaque LatLng, make GPS to LatLng

--- src/LatLng.elm
+++ src/LatLng.elm
@@ -1,7 +1,70 @@
-module LatLng exposing (LatLng)
+module LatLng exposing (GPS, LatLng, fromGps, latLng)
 
 
-type alias LatLng =
-    { lat : Float
-    , lng : Float
-    }
+type LatLng
+    = LatLng
+        { lat : Float
+        , lng : Float
+        }
+    | Gps
+        { lat : GPS
+        , lng : GPS
+        }
+
+
+type GPS
+    = GPS Float Float
+
+
+latLng : { lat : Float, lng : Float } -> LatLng
+latLng latlng =
+    LatLng latlng
+
+
+{-| test:
+n = GPS 49 34.658
+e = GPS 17 39.872
+jerryTakeOff = {lat = n, lng = e}
+
+    LatLng should be
+        (49.577633, 17.664533)
+    Full GPS data should be
+        49° 34' 39.4788'' N
+        17° 39' 52.3188'' E
+
+    Doublecheck on https://www.latlong.net/Show-Latitude-Longitude.html
+
+-}
+decGps : GPS -> Float
+decGps gps =
+    case gps of
+        GPS degree minute ->
+            degree + (minute / 60)
+
+
+fromGps : { lat : GPS, lng : GPS } -> LatLng
+fromGps { lat, lng } =
+    LatLng
+        { lat = decGps lat
+        , lng = decGps lng
+        }
+
+
+getLat : LatLng -> Float
+getLat latlng =
+    case latlng of
+        LatLng { lat, lng } ->
+            lat
+
+        Gps { lat, lng } ->
+            decGps lat
+
+
+getLng : LatLng -> Float
+getLng latlng =
+    case latlng of
+        LatLng { lat, lng } ->
+            lng
+
+        Gps { lat, lng } ->
+            decGps lng
