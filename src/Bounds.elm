40b9f947d1d5b1ee73e5865740e7024967d4e07c
kubaracek <me@jakubracek.net>
2020-02-23 13:34:02 +0100

    Centered bounds

--- src/Bounds.elm
+++ src/Bounds.elm
@@ -9,49 +9,60 @@ import LatLng as LatLng exposing (LatLng, getLat, getLng, latLng)
 import Tile exposing (Offset)
 import Utils exposing (wrap)
 
-
-type alias Bounds a =
+type Bounds a
+  = Bounds
     { northEast : a
     , southWest : a
     }
-
+  | Centered
+    { zoom : Float
+    , center : a
+    }
 
 type alias Zoom =
     Float
 
 
 zoom : Float -> Float -> Float -> Bounds LatLng -> Zoom
-zoom tileSize mapWidth mapHeight bounds =
-    let
-        latY lat =
-            sin (lat * pi / 180)
+zoom tileSize mapWidth mapHeight bounds_ =
+    case bounds_ of
+        Centered bounds ->
+            bounds.zoom
+        Bounds bounds ->
+            let
+                latY lat =
+                    sin (lat * pi / 180)
 
-        ( ne, sw ) =
-            ( bounds.northEast, bounds.southWest )
+                ( ne, sw ) =
+                    ( bounds.northEast, bounds.southWest )
 
-        radX2 lat =
-            logBase e ((1 + latY lat) / (1 - latY lat)) / 2
+                radX2 lat =
+                    logBase e ((1 + latY lat) / (1 - latY lat)) / 2
 
-        latRad lat =
-            (max -pi <| min (radX2 lat) pi) / 2
+                latRad lat =
+                    (max -pi <| min (radX2 lat) pi) / 2
 
-        latFraction =
-            latRad (getLat ne) - latRad (getLat sw)
+                latFraction =
+                    latRad (getLat ne) - latRad (getLat sw)
 
-        lngFraction =
-            ((getLng ne - getLng sw) |> wrap 0 360) / 360
+                lngFraction =
+                    ((getLng ne - getLng sw) |> wrap 0 360) / 360
 
-        zoomFn mapSize tileSize2 frac =
-            logBase 2 (mapSize / tileSize2 / frac)
-    in
-    min
-        (zoomFn mapWidth tileSize lngFraction)
-        (zoomFn mapHeight tileSize latFraction)
+                zoomFn mapSize tileSize2 frac =
+                    logBase 2 (mapSize / tileSize2 / frac)
+            in
+            min
+                (zoomFn mapWidth tileSize lngFraction)
+                (zoomFn mapHeight tileSize latFraction)
 
 
 findCenter : Bounds LatLng -> LatLng
-findCenter bounds =
-    latLng
-        { lat = (getLat bounds.northEast + getLat bounds.southWest) / 2
-        , lng = (getLng bounds.northEast + getLng bounds.southWest) / 2
-        }
+findCenter bounds_ =
+    case bounds_ of
+        Centered bounds ->
+            bounds.center
+        Bounds bounds ->
+            latLng
+                { lat = (getLat bounds.northEast + getLat bounds.southWest) / 2
+                , lng = (getLng bounds.northEast + getLng bounds.southWest) / 2
+                }
